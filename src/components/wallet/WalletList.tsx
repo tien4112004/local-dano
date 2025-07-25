c9d837176834e1b51c44e248dae77a635a12e058
openVietAnh <vietanhtran.uet@gmail.com>
2025-07-25 13:45:28 +0700

    switch back to http for API call

--- src/components/wallet/WalletList.tsx
+++ src/components/wallet/WalletList.tsx
@@ -32,7 +32,7 @@ export const WalletList = ({
 
   const fetchWallets = async () => {
     try {
-      const response = await fetch("https://172.16.61.201:8090/v2/wallets");
+      const response = await fetch("http://172.16.61.201:8090/v2/wallets");
       if (!response.ok) {
         throw new Error("Failed to fetch wallets");
       }
